d57db960e8b4866c84c3548fc4c8802b686a57f4
Saraswatiyadavcs24 <saraswatiyadav.cs24@bmsce.ac.in>
2025-12-22 09:06:03 +0530

    Implement DFS to check graph connectivity

---
+++ lab prog 9b/code.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define MAX 10
+
+int visited[MAX];
+int adj[MAX][MAX];
+int n;
+
+
+void DFS(int v) {
+    visited[v] = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (adj[v][i] == 1 && !visited[i]) {
+            DFS(i);
+        }
+    }
+}
+
+int main() {
+    int connected = 1;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+
+    DFS(0);
+
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            connected = 0;
+            break;
+        }
+    }
+
+    if (connected)
+        printf("The graph is CONNECTED\n");
+    else
+        printf("The graph is NOT CONNECTED\n");
+
+    return 0;
+}
